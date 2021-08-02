73cc6ca2dbd1acb271e73c6dd57078651ffd2dbb
ystime25 <ystime25@gmail.com>
2021-08-02 12:14:47 +0900

    Basic CSS Settings

--- src/server.js
+++ src/server.js
@@ -5,8 +5,13 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
 const handleListen = () => {
   console.log(`✅ Server Online => http://localhost:${PORT}`);
 };
-
 app.listen(PORT, handleListen);
